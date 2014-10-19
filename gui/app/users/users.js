0142d488727132ee33d59cbe29ac857c8275fb80
Thomas Bindzau <thomas.bindzau@gmail.com>
2014-10-19 12:21:23 +0200

    x

--- gui/app/users/users.js
+++ gui/app/users/users.js
@@ -11,38 +11,23 @@ angular.module('myApp.users', ['ngRoute'])
 
 .controller('UsersCtrl', ['$scope', '$http', function(scope, http) {
 
-/*
-	scope.myData = [
-        {
-            "firstName": "Cox",
-            "lastName": "Carney",
-            "userName": "cc",
-            "email":  "cox@carney.se",
-            "createdDate": "2014-01-01",
-            "access": "true"
-        },
-        {
-            "firstName": "Thomas",
-            "lastName": "Bindzau",
-            "userName": "tb",
-            "email":  "thomas@bindzau.se",
-            "createdDate": "2014-08-04",
-            "access": "false"
-        },
-        {
-            "firstName": "John",
-            "lastName": "Doe",
-            "userName": "jd",
-            "email":  "john@doe.se",
-            "createdDate": "2014-06-12",
-            "access": "true"
-        }];
-*/
-
+//Note: problem with Swedish chars
+scope.gridOptions = {
+        enableSorting: true,
+        columnDefs: [
+          { name:'Förnamn', field: 'firstName' },
+          { name:'Efternamn', field: 'lastName' },
+          { name:'Anvaendarnamn', field: 'uesrName' },
+          { name:'E-post', field: 'email' },
+          { name:'Behörighet', field: 'access' }
+        ],
+        data : []
+      };
+ 
 var loadUsers = function(){
  http({ method: 'GET', url: 'http://localhost:8080/users' }).
   success(function (data, status, headers, config) {
-    scope.myData = data;
+    scope.gridOptions.data = data;
   }).
   error(function (data, status, headers, config) {
     alert("Data:" + data + ", Status" + status);
